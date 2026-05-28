6cc53e98df8caf70330dd57b7a07937fbdb8dd1f
ateiken <teiken.t.96@gmail.com>
2026-05-28 16:02:25 -0500

    device connect

---
+++ device_connect.py
@@ -0,0 +1,18 @@
+from netmiko import ConnectHandler
+
+# define device connection params
+device = {
+    'device_type': 'arista_ios',
+    'host': '192.168.8.101',
+    'username': 'ansible',
+    'password': 'automation',
+}
+
+# connect to device
+connection = ConnectHandler(**device)
+
+# run a command
+output = connection.send_command('show version')
+print(output)
+
+connection.disconnect()
