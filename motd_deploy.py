a83388650891cfa8106f6275021fa2a659519b38
ateiken <teiken.t.96@gmail.com>
2026-06-08 10:42:38 -0500

    syntax update

--- motd_deploy.py
+++ motd_deploy.py
@@ -26,9 +26,11 @@ for device in devices:
 
         # define config to push
         config_commands = [
-            'banner motd @',
-            'Managed by Netmiko',
-            '@'
+            'banner motd ^',
+            'Unauthorized access is prohibited.',
+            'All activities are monitored and recorded.',
+            'Disconnect immediately if you are not an authorized user.',
+            '^'
         ]
         
         output = conn.send_config_set(config_commands)
